98876c55999b722c1e45944b6dcfff465d02a222
amitvsolutions <97821655+amitvsolutions@users.noreply.github.com>
2023-02-28 09:13:54 +0530

    update in day-1 code

--- Phase-1/Day-1.java
+++ Phase-1/Day-1.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class Day1{
+	
 	public static void main(String[] args){
 		int[] items = { 10, 0, 30, 2, 7, 5, 90, 76, 100, 45, 55 };   // Min = 0, Max = 100
 		
 		// Approach-1
-		max = items[0];
-		min = items[0];
+		int max = items[0];
+		int min = items[0];
 
 		for (int i = 1; i < items.length; i++) {
 			if(items[i]>max){
@@ -17,9 +20,11 @@ public class Day1{
 		System.out.println(min);	//0
 		
 		// Approach-2
-		int max = Arrays.stream(items).max().getAsInt(); // 100
-
-		int min = Arrays.stream(items).min().getAsInt(); // 0
+		max = Arrays.stream(items).max().getAsInt(); // 100
+		min = Arrays.stream(items).min().getAsInt(); // 0
+		
+		System.out.println(max);	//100
+		System.out.println(min);	//0
 		
 		// Approach-3: Try with Recursion !!!
 		
@@ -33,4 +38,6 @@ public class Day1{
 		set max = current item
 	    elseif current item is lower than min
 	 	set min = current item*/
+
 }
+
