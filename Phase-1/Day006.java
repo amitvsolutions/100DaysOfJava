2bd4ddd19377fb5bb44ac18c188853cbcf9c1e7b
amitvsolutions <amitverma.ibmindia@gmail.com>
2023-03-04 12:43:04 +0530

    java break vs continue in loop

---
+++ Phase-1/Day006.java
@@ -0,0 +1,17 @@
+public class Day006{
+	
+   public static void main(String args[]){
+      // Illustrating break statement (execution stops when value of i becomes to 4.)
+      System.out.println("Break Statement\n");
+      for(int i=1;i<=9;i++){
+         if(i==4) break;
+         System.out.println(i);
+      }
+      // Illustrating continue statement (execution skipped when value of i becomes to 1.)
+      System.out.println("Continue Statement\n");
+      for(int i=1;i<=9;i++){
+         if(i==4) continue;
+         System.out.println(i);
+      }
+   }	
+}
