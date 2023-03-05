c272101c25b34fd9de02f83aa2ec0766977188db
amitvsolutions <amitverma.ibmindia@gmail.com>
2023-03-05 20:58:09 +0530

    read input from console

--- Phase-1/Day007.java
+++ Phase-1/Day007.java
@@ -7,29 +7,32 @@ public class Day007{
 	
    // Read input from console //
    public static void main(String args[]){
-	   
 	   // Appraoch-1: 
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-        System.out.println("Enter anything: ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		System.out.println("Enter anything: ");
 		
-        String str = br.readLine();
-		while(!str.equals("stop")){
+		String input = br.readLine();
+		if(input.equals("stop")){
+			br.close(); 
+		}else {
 			System.out.println("You have Entered: ");
-			System.out.println(str);  
+			System.out.println(input); 				
 		}
-		br.close();        
+				
 	   
 	   // Approach-2: 
 	   Scanner sc = new Scanner(System.in);
 	   System.out.println("Enter a String");
-       String str = sc.nextLine();
-       System.out.println("The String is: " +str);
-       System.out.println("Enter an Integer");
-       int i = sc.nextInt();
-       System.out.println("The Integer is: " +i);
-       System.out.println("Enter a Float value");
-       float f = sc.nextFloat();
-       System.out.println("The Float value is: " +f);
+	   String str = sc.nextLine();
+	   System.out.println("The String is: " +str);
+	   System.out.println("Enter an Integer");
+	   int i = sc.nextInt();
+	   System.out.println("The Integer is: " +i);
+	   System.out.println("Enter a Float value");
+	   float f = sc.nextFloat();
+	   System.out.println("The Float value is: " +f);
+	   sc.close();
+	   }	
    }	
 }
 
